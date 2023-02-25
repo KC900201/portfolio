5a60a52959f461b40f266ea823450c63952f49d0
KC Ng <nkc900201@gmail.com>
2023-02-25 16:43:06 +0900

    Finished incorporating dark mode in portfolio. Change js file loading into "defer" to be downloaded in parallel with html Implement @mixin to toggle dark-mode / light-mode Add in $buttonBgColor in _config

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
+// Reference 
+// https://javascript.plainenglish.io/how-to-create-a-dark-mode-with-sass-scss-and-vanilla-javascript-e1c7835cf474
+// https://codesandbox.io/s/scss-dark-mode-stage3-ug2w0i?from-embed=&file=/src/styles.scss
+
 const btn = document.querySelector(".btn-toggle");
+const body = document.body;
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-btn.addEventListener("click", function () {
-    if (prefersDarkScheme.matches) {
-        document.body.classList.toggle("light-theme")
-    } else {
-        document.body.classList.toggle("dark-theme")
-    }
-})
+
+const addDarkMode = () => {
+    body.classList.remove("light-mode");
+    body.classList.add("dark-mode");
+}
+
+const addLightMode = () => {
+    body.classList.remove("dark-mode");
+    body.classList.add("light-mode");
+}
+
+const toggleTheme = () => {
+    !body.classList.contains("dark-mode") ? addDarkMode() : addLightMode();
+}
+
+const checkPreference = () => {
+    prefersDarkScheme.matches ? addDarkMode() : addLightMode();
+}
+
+btn.addEventListener("click", toggleTheme);
+prefersDarkScheme.addEventListener("change", checkPreference);
+(() => checkPreference())();
